44bc7aec4926acbe25f30421d6678ee07f968f46
Zeon8 <dimayaremko@ukr.net>
2024-11-07 17:51:04 +0200

    Use StringFormat property for price.

--- CryptoCoinViewer/ViewModels/AssetViewModel.cs
+++ CryptoCoinViewer/ViewModels/AssetViewModel.cs
@@ -6,8 +6,6 @@ public class AssetViewModel
 {
     public Asset Asset { get; }
 
-    public string PriceUsd => "$" + Asset.PriceUsd;
-
     public string Image => $"https://assets.coincap.io/assets/icons/{Asset.Symbol.ToLower()}@2x.png";
     
     public AssetViewModel(Asset asset)
